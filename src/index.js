c22d6b7c007d788f8dd9321871984fa3e76e7a90
Andrew Rogulev <6220076@gmail.com>
2020-10-03 19:35:03 -0700

    init:  my task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  return (typeof array == "undefined" || array.length <= 0) ? 0
+    : array.reduce((min, current) => {
+      return (min < current) ? min : current;
+    });
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  return (typeof array == "undefined" || array.length <= 0) ? 0
+    : array.reduce((max, current) => {
+      return (max > current) ? max : current;
+    });
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (typeof array == "undefined" || array.length <= 0) {
+    return 0;
+  } else {
+    let full = 0;
+    let avgNumber = 0;
+
+    for (let i = 0; i < array.length; i++) {
+      full += array[i];
+    }
+    return avgNumber = full / array.length;
+  }
 }
